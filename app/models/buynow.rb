d918f0230c26621683c35f1555f00dff8ada70c3
k-nowicki <karol@knowicki.pl>
2012-02-09 23:51:37 +0100

    last improvements

--- app/models/buynow.rb
+++ app/models/buynow.rb
@@ -1,7 +1,5 @@
 class Buynow < Auction
 
-  #scope :order => "endtime DESC"
-
   # - kup teraz - ma wyświetlać nazwę dużymi literami z trzema wykrzyknikami dodatkowo na końcu
   def pretty_name
     "#{name.upcase}!!!"
--- app/models/snail.rb
+++ app/models/snail.rb
@@ -1,5 +1,5 @@
 class Snail < Auction
-  default_scope :order => :endtime
+
   # - ślimacza - ma wyświetlać nazwę dużymi literami ze znakami @@ na początku
   def pretty_name
     "@@#{name.upcase}"
