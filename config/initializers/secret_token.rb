441d50df8aee5fc79adef70df9d8f6e257cf3671
k-nowicki <karol@knowicki.pl>
2012-02-09 10:21:07 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microebay::Application.config.secret_token = 'b6c81c224d1ca6cd1a56f93d40d237f8288dbbf8d29915b472bc3712a7abaf8544d6d1f08049bdd2c2d6776e065f7480eec5f11705cccdb1d7f58ec09fd8d763'
